9a583485622cae2c601c432d594afc0826ad3617
Lucas Zamora <lucas@phac.com>
2024-11-06 22:12:26 -0300

    feat: ensure transaction table labels are rendered property

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -11,7 +11,7 @@ const TransactionsPage = async () => {
     <div className="p-6 space-y-6">
       <div className="flex w-full justify-between items-center">
         <h1 className="font-bold text-2xl">Transações</h1>
-        <Button className="rounded-full">
+        <Button className="rounded-full font-bold">
           Adicionar Transação
           <ArrowDownUpIcon />
         </Button>
