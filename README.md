f1f6e2e888af4565660b20c5dbc6a67a35a955b9
Lucas Zamora <lucas@phac.com>
2024-11-06 21:47:52 -0300

    feat: add transaction type badge

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 -- npx prisma init
 -- npx prisma format
 -- npx prisma migrate dev --name init_db
+-- npx prisma studio
---
+++ app/transactions/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionsColumns } from "./_columns/index.tsx";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({}); //findAll do prisma
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex w-full justify-between items-center">
+        <h1 className="font-bold text-2xl">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionsColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
