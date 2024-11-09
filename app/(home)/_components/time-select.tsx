82b927e532bc2dcf3e69f5014332b9e6d3d2d113
Lucas Zamora <lucas@phac.com>
2024-11-09 14:38:27 -0300

    feat: add summary cards

---
+++ app/(home)/_components/time-select.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/_components/ui/select";
+import { MONTH_OPTIONS } from "@/app/_constants/home";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const TimeSelect = () => {
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
+  const month = searchParams.get("month");
+  const handleMonthChange = (month: string) => {
+    push(`/?month=${month}`);
+  };
+  return (
+    <Select
+      onValueChange={(value) => handleMonthChange(value)}
+      defaultValue={month ?? ""}
+    >
+      <SelectTrigger className="w-[150px] rounded-full">
+        <SelectValue placeholder="Mês" />
+      </SelectTrigger>
+      <SelectContent>
+        {MONTH_OPTIONS.map((item) => (
+          <SelectItem key={item.value} value={item.value}>
+            {item.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default TimeSelect;
