14a9383345e2ba01876071fe2242be76e2b22ada
Lucas Zamora <lucas@phac.com>
2024-11-11 23:09:28 -0300

    fix: remove components that are not being used in the columns file

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,8 +3,6 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
-import { Button } from "@/app/_components/ui/button";
-import { TrashIcon } from "lucide-react";
 import {
   TRANSACTION_CATEGORY_LABELS,
   TRANSACTION_PAYMENT_METHOD_LABELS,
